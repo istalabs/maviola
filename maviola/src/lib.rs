f3087538af1c658be1e067695a97ccf188dc7686
Mykhailo Ziatin <mikhail.zyatin@gmail.com>
2023-12-30 16:21:29 +0200

    Add WIP clause to API docs.

--- maviola/src/lib.rs
+++ maviola/src/lib.rs
@@ -4,6 +4,20 @@
 //! provides abstractions like communication nodes and namespaces and takes care of **stateful**
 //! features of the MAVLink protocol, such as sequencing, message time-stamping, automatic
 //! heartbeats, simplifies message signing, and so on.
+//!
+//! Maviola is based on [Mavio](https://gitlab.com/mavka/libs/mavio), a low-level library with `no-std` support. If you
+//! are looking for a solution for embedded devices, then Mavio would be a better option.
+//!
+//! > **⚠ WIP**
+//! >
+//! > Maviola is still under heavy development. The aim is to provide API similar to
+//! > [`gomavlib`](https://github.com/bluenviron/gomavlib) with additional support for essential MAVLink
+//! > ["microservices"](https://mavlink.io/en/services/) such as [heartbeat](https://mavlink.io/en/services/heartbeat.html),
+//! > [parameter protocol](https://mavlink.io/en/services/parameter.html) and
+//! > [commands](https://mavlink.io/en/services/command.html).
+//! >
+//! > This is project stub. We intentionally do not publish early versions of API to avoid confusion and massive
+//! > breaking changes.
 
 #![warn(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
